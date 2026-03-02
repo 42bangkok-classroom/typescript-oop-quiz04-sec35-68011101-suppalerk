85590167f409f026682c93b31632c7a13f2acac2
68011101-suppalerk <68011101@kmitl.ac.th>
2026-03-02 10:03:03 +0700

    5

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,3 +1,9 @@
-class UserService {
-    
+import { UserService } from './user.service';
+
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  getUsers(): string []{
+    return this.userService.getUsers();
+  }
 }
