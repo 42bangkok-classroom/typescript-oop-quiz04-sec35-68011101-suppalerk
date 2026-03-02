aef7198201ca173079c1db60614a32fef0b29990
68011101-suppalerk <68011101@kmitl.ac.th>
2026-03-02 09:51:58 +0700

    2

---
+++ src/user/user.service.ts
@@ -0,0 +1,9 @@
+class UserService {
+        message: string = "Cannot GET /users/test"
+    error: string = "Not Found"
+    statusCode: number = 404
+
+      get user(){
+    return this.user;
+  }
+}
