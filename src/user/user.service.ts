ab664f438ffe20c3fb34876e248559482423add1
68011101-suppalerk <68011101@kmitl.ac.th>
2026-03-22 16:13:14 +0700

    test 8

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,9 +2,14 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class UserService {
-
+    id: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    username: string;
   test(): {
-  } {
-    return [];
+  }
+  {
+  return [];
   }
 }
