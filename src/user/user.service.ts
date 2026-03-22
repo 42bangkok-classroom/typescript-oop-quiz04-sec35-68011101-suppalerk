08c40f32a64f4abbfdc986757ea263c2ecdd0773
68011101-suppalerk <68011101@kmitl.ac.th>
2026-03-22 17:56:37 +0700

    re test 2

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { IUser } from './user.interface';
+// import { IUser } from './user.interface';
 export interface User {
   id: string;
   firstName: string;
