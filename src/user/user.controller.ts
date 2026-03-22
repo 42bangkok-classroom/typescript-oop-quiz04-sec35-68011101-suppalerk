53310156d4b9680c45d09451151cce479534dec5
68011101-suppalerk <68011101@kmitl.ac.th>
2026-03-22 12:06:09 +0700

    test2

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -5,7 +5,7 @@ export class UserController {
   constructor(private readonly UserService: UserService) {}
 
   @Get()
-  test() {
+  test() : {id : string , firstname : string , lastname : string , email : string , username : string}[] {
     return this.UserService.test();
   }
 
