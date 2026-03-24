0445452e10cd5d8373b60b8d5b8ac14cb1665bb7
68011101-suppalerk <68011101@kmitl.ac.th>
2026-03-25 04:54:58 +0700

    test13

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -33,7 +33,7 @@ export class UserService {
     if (!user) {
       throw new NotFoundException('User not found');
     }
-    if (fields && fields.length > 0) {
+    if (fields) {
       const filteredUser: Partial<IUser> = {};
 
       fields.forEach((fields: string) => {
